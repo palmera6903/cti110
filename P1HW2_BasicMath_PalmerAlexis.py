88536bd4def778abd073bee7c63a7a7a085df628
palmera6903 <71239659+palmera6903@users.noreply.github.com>
2020-09-16 18:14:49 -0400

    Add files via upload
    Module 2 P1HW2
    Adding and multiplying integers.

---
+++ P1HW2_BasicMath_PalmerAlexis.py
@@ -0,0 +1,14 @@
+#Adding and multiplying integers
+#9/16/2020
+#CTI-110 P1HW2-Basic Math
+#Alexis Palmer
+#
+print('Enter first number:', end=' ')
+first_number = int(input())
+print('Enter second number:', end=' ')
+second_number = int(input())
+print('First number entered:', first_number)
+print('Second number entered:', second_number)
+print()
+print('Sum of both numbers:', first_number + second_number)
+print('Result fo Multiplying the two numbers:', first_number * second_number)
